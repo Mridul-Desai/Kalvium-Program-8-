2744b285cf9008994f8d9982948f90c72ede82fc
Mridul-Desai <mridul.d2002@gmail.com>
2025-03-12 15:47:21 +0530

    updated

--- src/components/Darkbutton.jsx
+++ src/components/Darkbutton.jsx
@@ -6,6 +6,10 @@ function Darkbutton() {
 
   // TASK 4: Implement the logic for changing the background and text color of the entire page.
   const toggleDarkMode = () => {
+    document.body.style.background=setIsDarkMode('rgb(24, 24, 24)')
+    document.body.style.color=setIsDarkMode('rgb(240,240,240)')
+
+    
     // When dark mode is active, change background to black and text to white.
     // When light mode is active, change background to white and text to black.
   };
--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -5,12 +5,12 @@ const Result = ({ score, len, restartQuiz }) => {
   return (
     <div className="result-box">
       <h2>Quiz Completed!</h2>
-      {/* Task 5.1: Display the score with the total number of questions */}
+      {score}
       <p>Your Score:</p>
       {/* Task 5.2: Display a custom message based on the user's score:
           - If the percentage is greater than 50%, show "Great job!"
           - If the percentage is 50% or less, show "Better luck next time!"*/}
-      <p>{/* Custom message here */}</p>
+      <p>{"Great job!"}</p>
       <button className="restart-btn" onClick={restartQuiz}>
         Restart Quiz
       </button>
